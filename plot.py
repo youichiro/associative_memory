342a0babcf2cb85ddedf2dccc1f0e7612f3d0d5e
youichiro <cinnamon416@gmail.com>
2018-10-21 12:04:09 +0900

    fix

---
+++ plot.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+filename = 'log.txt'
+
+logs = open(filename).readlines()
+logs = logs[1:]
+
+L_list = [int(log.split('\t')[0]) for log in logs]
+acc_list = [int(log.split('\t')[1].replace('\n', '')) for log in logs]
+
+plt.plot(L_list, acc_list)
+plt.xlabel('L')
+plt.ylabel('平均成功率[%]')
+plt.ylim(0.0, 110.0)
+plt.show()
